102b6d0b6088296142a8050ccc81b03a2113fd62
Om Patil <omp.omtech@gmail.com>
2024-06-08 14:00:34 +0530

    Small change in /api/getbids

--- src/app/api/getbids/route.ts
+++ src/app/api/getbids/route.ts
@@ -4,7 +4,7 @@ import { NextRequest } from "next/server";
 export async function GET(req:NextRequest){
     const client = await clientPromise
     const db = client.db("BITSBids")
-    const products = await db.collection('products').find({category:'other'}).toArray()
+    const products = await db.collection('products').find({}).toArray()
     console.log((products))
     return new Response(JSON.stringify({products:products}))
 }
